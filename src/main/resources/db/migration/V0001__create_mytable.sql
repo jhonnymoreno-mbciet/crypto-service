a831917c9f58932b0bf57ec29890b5d48c22532e
Wesley Ruben Oliveira da Silva <wesleysilva@ciandt.com>
2022-04-13 01:29:03 -0300

    Configurando gradle build, application.yml e criando script da tabela currency.

--- src/main/resources/db/migration/V0001__create_mytable.sql
+++ src/main/resources/db/migration/V0001__create_mytable.sql
@@ -1,5 +1,12 @@
-create table if not exists mytable (
-    id serial primary key,
-    columnOne varchar,
-    columnTwo numeric
+CREATE TABLE IF NOT EXISTS currency(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    code VARCHAR(80) NOT NULL,
+	createdAt TIMESTAMP
 );
+
+INSERT INTO currency(name, code, createdAt) VALUES('Bitcoin', 'BTC', current_timestamp);
+INSERT INTO currency(name, code, createdAt) VALUES('Ethereum', 'ETH', current_timestamp);
+INSERT INTO currency(name, code, createdAt) VALUES('Tether', 'USDT', current_timestamp);
+INSERT INTO currency(name, code, createdAt) VALUES('USD Coin', 'USDC', current_timestamp);
+INSERT INTO currency(name, code, createdAt) VALUES('SOLANA', 'SOL', current_timestamp);
